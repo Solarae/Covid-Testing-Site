ccce992d3776b876627ef2dcfbd8ff961783e096
unknown <ronnie.chen@stonybrook.edu>
2020-11-25 15:41:28 -0500

    Changed the URL to connect to MongoDBAtlas database

--- client/src/components/TestCollection.js
+++ client/src/components/TestCollection.js
@@ -8,7 +8,8 @@ class TestCollection extends React.Component {
         testBarcode: "",
         tests: [{_id: 1, employeeID: "Ronnie", testBarcode: "123"},
         {_id: 2, employeeID: "Singwa", testBarcode: "456"}],
-        isLoading: false
+        isLoading: false,
+        toDelete: []
     }
 
     componentDidMount() {
@@ -43,8 +44,29 @@ class TestCollection extends React.Component {
             })
     }
 
+    handleCheckClick = (id, e) => {
+        if (e.target.checked) {
+            this.setState( {
+                toDelete: [id, ...this.state.toDelete]
+            } )
+        } else {
+            this.setState( {
+                toDelete: this.state.toDelete.splice(this.state.toDelete.indexOf(id), 1)
+            } )
+        }
+        
+    }
+
+    deleteClick = () => {
+        this.state.toDelete.forEach(del => {
+            this.setState( {
+                tests: this.state.tests.filter(test => test._id !== del)
+            } )
+        })
+    }
+
     renderTableHeader() {
-        const header = ["Employee ID", "Test Barcode"]
+        const header = ["Checkbox", "Employee ID", "Test Barcode"]
         return header.map((hd) => {
             return <th key={`Header ${hd}`}>{hd}</th>
         })
@@ -54,6 +76,11 @@ class TestCollection extends React.Component {
         return this.state.tests.map(({_id, employeeID, testBarcode}, index) => {
            return (
               <tr key={_id}>
+                <td>
+                    <FormGroup check>
+                        <Input type="checkbox" defaultChecked = "false" onChange={this.handleCheckClick.bind(this,_id)}/>
+                    </FormGroup>
+                 </td>
                  <td>{employeeID}</td>
                  <td>{testBarcode}</td>
               </tr>
@@ -97,6 +124,11 @@ class TestCollection extends React.Component {
                         <thead><tr>{this.renderTableHeader()}</tr></thead>
                         <tbody>{this.renderTableData()}</tbody>
                     </Table>
+                </div>
+                <div className="text-center">
+                    <Button onClick={this.deleteClick()}>
+                        Delete
+                    </Button>
                 </div>
             </Container>
         )
