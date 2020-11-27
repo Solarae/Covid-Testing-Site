23f6f79c3447cab3d38c141aae6d2790236095f6
unknown <ronnie.chen@stonybrook.edu>
2020-11-27 15:20:28 -0500

    Changed the Schema for PoolMap

--- models/PoolMap.js
+++ models/PoolMap.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const PoolMapSchema = new Schema({
-    testBarcode: {
-        type: String,
-        maxlength: 50,
-        required: true
-    }, 
     poolBarcode: {
         type: String,
         maxlength: 50,
         required: true
-    }
+    },
+    testBarcodes: {
+        type: Array,
+        required: true
+    }, 
+    
 })
 
 module.exports = PoolMap = mongoose.model('poolmap', PoolMapSchema)
