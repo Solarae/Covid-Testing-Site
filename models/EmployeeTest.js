d94cd0a237ebc9216736e0868ee875ca3c4028c5
unknown <ronnie.chen@stonybrook.edu>
2020-11-30 12:50:27 -0500

    Fixed the Employee schema

--- models/EmployeeTest.js
+++ models/EmployeeTest.js
@@ -3,15 +3,16 @@ const Schema = mongoose.Schema
 const Test = require('./Test')
 
 const EmployeeTestSchema = new Schema({
+    _id: {
+        type: String,
+        maxlength: 20,
+    },
     testBarcodes: [{
         type: Schema.Types.ObjectId, 
         ref: Test
     }],
-    employeeID: {
-        type: String,
-        maxlength: 20,
-        required: true
-    }
 })
+
+EmployeeTestSchema.virtual('employeeID').get(() => {return this._id})
 
 module.exports = EmployeeTest = mongoose.model('employeeTest', EmployeeTestSchema)
